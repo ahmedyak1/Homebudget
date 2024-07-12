c0b0720e80a9d242eb1d6e6209422bfb28e8be56
ahmed yakine <132367513+ahmedyak1@users.noreply.github.com>
2024-07-12 02:04:29 +0100

    logoutAction function for improved readability

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,24 +1,18 @@
-// rrd imports
+// Import necessary libraries
 import { redirect } from "react-router-dom";
-
-// library
 import { toast } from "react-toastify";
-
-// helpers
 import { deleteItem } from "../helpers";
 
+// Function to handle user logout and account deletion
 export async function logoutAction() {
-  // delete the user
-  deleteItem({
-    key: "userName"
-  })
-  deleteItem({
-    key: "budgets"
-  })
-  deleteItem({
-    key: "expenses"
-  })
-  toast.success("You’ve deleted your account!")
-  // return redirect
-  return redirect("/")
+  // Delete user data from local storage
+  deleteItem({ key: "userName" });
+  deleteItem({ key: "budgets" });
+  deleteItem({ key: "expenses" });
+
+  // Notify the user of successful account deletion
+  toast.success("You’ve deleted your account!");
+
+  // Redirect to the home page after deletion
+  return redirect("/");
 }
