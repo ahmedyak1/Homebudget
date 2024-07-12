c26ee92e583173f9602c465a5f941b0beadfeed3
ahmed yakine <132367513+ahmedyak1@users.noreply.github.com>
2024-07-12 02:22:14 +0100

    Update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// Import necessary libraries and components
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
+// Render the main application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
