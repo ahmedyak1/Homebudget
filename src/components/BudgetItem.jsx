f0ce03d0b868dd48f1f746480f6226281723237c
ahmed yakine <132367513+ahmedyak1@users.noreply.github.com>
2024-07-12 02:13:14 +0100

    Update  BudgetItem component

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -11,54 +11,44 @@ import {
 const BudgetItem = ({ budget, showDelete = false }) => {
   const { id, name, amount, color } = budget;
   const spent = calculateSpentByBudget(id);
+  const remaining = amount - spent;
+  const spentPercentage = spent / amount;
+
+  // Handle delete confirmation
+  const handleDelete = (event) => {
+    if (!confirm("Are you sure you want to permanently delete this budget?")) {
+      event.preventDefault();
+    }
+  };
 
   return (
-    <div
-      className="budget"
-      style={{
-        "--accent": color,
-      }}
-    >
+    <div className="budget" style={{ "--accent": color }}>
       <div className="progress-text">
         <h3>{name}</h3>
         <p>{formatCurrency(amount)} Budgeted</p>
       </div>
       <progress max={amount} value={spent}>
-        {formatPercentage(spent / amount)}
+        {formatPercentage(spentPercentage)}
       </progress>
       <div className="progress-text">
         <small>{formatCurrency(spent)} spent</small>
-        <small>{formatCurrency(amount - spent)} remaining</small>
+        <small>{formatCurrency(remaining)} remaining</small>
       </div>
-      {showDelete ? (
-        <div className="flex-sm">
-          <Form
-            method="post"
-            action="delete"
-            onSubmit={(event) => {
-              if (
-                !confirm(
-                  "Are you sure you want to permanently delete this budget?"
-                )
-              ) {
-                event.preventDefault();
-              }
-            }}
-          >
+      <div className="flex-sm">
+        {showDelete ? (
+          <Form method="post" action="delete" onSubmit={handleDelete}>
             <button type="submit" className="btn">
               <span>Delete Budget</span>
               <TrashIcon width={20} />
             </button>
           </Form>
-        </div>
-      ) : (
-        <div className="flex-sm">
+        ) : (
           <Link to={`/budget/${id}`} className="btn">
             <span>View Details</span>
             <BanknotesIcon width={20} />
           </Link>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
