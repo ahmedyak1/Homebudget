e94ec904d8d70078552fcf44aa71c5e168ba9c9b
ahmed yakine <132367513+ahmedyak1@users.noreply.github.com>
2024-07-12 02:16:13 +0100

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,42 +1,34 @@
-// rrd imports
-import { Form, NavLink } from "react-router-dom"
+// Import necessary libraries and components
+import { Form, NavLink } from "react-router-dom";
+import { TrashIcon } from '@heroicons/react/24/solid';
+import logomark from "../assets/logomark.svg";
 
-// library
-import { TrashIcon } from '@heroicons/react/24/solid'
-
-// assets
-import logomark from "../assets/logomark.svg"
-
+// Nav component for the navigation bar
 const Nav = ({ userName }) => {
   return (
     <nav>
-      <NavLink
-        to="/"
-        aria-label="Go to home"
-      >
-        <img src={logomark} alt="" height={30} />
+      <NavLink to="/" aria-label="Go to home">
+        <img src={logomark} alt="HomeBudget logo" height={30} />
         <span>HomeBudget</span>
       </NavLink>
-      {
-        userName && (
-          <Form
-            method="post"
-            action="logout"
-            onSubmit={(event) => {
-              if (!confirm("Delete user and all data?")) {
-                event.preventDefault()
-              }
-            }}
-          >
-            <button type="submit" className="btn btn--warning">
-              <span>Delete User</span>
-              <TrashIcon width={20} />
-            </button>
-
-          </Form>
-        )
-      }
+      {userName && (
+        <Form
+          method="post"
+          action="logout"
+          onSubmit={(event) => {
+            if (!confirm("Delete user and all data?")) {
+              event.preventDefault();
+            }
+          }}
+        >
+          <button type="submit" className="btn btn--warning">
+            <span>Delete User</span>
+            <TrashIcon width={20} />
+          </button>
+        </Form>
+      )}
     </nav>
-  )
-}
-export default Nav
+  );
+};
+
+export default Nav;
